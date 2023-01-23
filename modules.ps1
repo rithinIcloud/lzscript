7c277af98b0677f3f06d16749267425b4de210cc
rithinIcloud <123351569+rithinIcloud@users.noreply.github.com>
2023-01-23 09:00:41 +0300

    Create modules.ps1

---
+++ modules.ps1
@@ -0,0 +1,12 @@
+Install-Module Az -AllowClobber -Force
+Install-Module ImportExcel -AllowClobber -Force
+Install-Module Microsoft.Graph -AllowClobber -Force
+Install-Module Az.ResourceGraph -AllowClobber -Force
+Install-Module PSFramework -AllowClobber -Force
+
+
+Import-Module Az 
+Import-Module ImportExcel 
+Import-Module Microsoft.Graph 
+Import-Module Az.ResourceGraph 
+Import-Module PSFramework 
